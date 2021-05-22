ab602d717ed5abb18ee1254d9f1bbbcf0058d3fb
seagulls-lucky <seagulls.lucky125@gmail.com>
2021-05-23 02:25:42 +0900

    developer story 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+
 import SearchBar from './components/search-bar';
 import SongForm from './components/song-form';
 import MusicTable from './components/music-table';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [tableData, setTableData] = useState([]);
+  
+  const searchSong = (t) => {
+    setSearchText(t);
+  }
+
+  const handleAdd = (song) => {
+    console.log(song)
+  }
+
   return (
     <div className="App">
       <h1>Music Library React Project</h1>
-      <SearchBar />
-      <SongForm />
-      <MusicTable />
+      <br />
+      <SongForm onAdd={(song)=>handleAdd(song)}/>
+      <br />
+      <SearchBar value={searchText} onChange={(t)=>searchSong(t)}/>
+      <br />
+      <MusicTable search={searchText} data={tableData}/>
     </div>
   );
 }
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,18 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import React, { useState } from "react";
+import {
+	Label,
+	Input,
+	Row,
+	Col,
+} from "reactstrap";
 
 const SearchBar = (props) => {
+
+	const [text, setText] = useState(props.value);
+
+	const handleChange = (e) => {
+		props.onChange(e.target.value);
+		setText(e.target.value)
+	}
+
 	return (
 		<React.Fragment>
-			<div>
-				<Label for="search">Search</Label>
-				<Input
-					type="text"
-					name="search"
-					id="search"
-					placeholder="Search ... "
-				/>
-			</div>
+			<Row>
+				<Col lg={3}>
+					<Label for="search">Search</Label>
+				</Col>
+				<Col lg={9}>
+					<Input
+						type="text"
+						name="search"
+						id="search"
+						placeholder="Search ... "
+						value={text}
+						onChange={handleChange}
+					/>
+				</Col>
+			</Row>
 		</React.Fragment>
 	);
 };
